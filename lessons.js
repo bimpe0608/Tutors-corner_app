a2bf3d1a0059c146da629384ba7d8ce78ed295aa
bimpe <adeyinminuadebimpe@gmail.com>
2023-10-27 22:28:21 +0100

    created first lesson

---
+++ lessons.js
@@ -0,0 +1,24 @@
+// new vue({
+//   el: "app",
+//   data: {
+//     sitename: "Tutor's Corner After School Class Lessons",
+//     product: {
+//         id: 100,
+//         Subject: "Math",
+//         Location: "Manchester",
+//         Price: 30,
+//         imagePath:
+//     }
+//   },
+// });
+
+let products = [
+  {
+    id: 100,
+    subject: "Trigonometry",
+    location: "Chelsea",
+    price: "100",
+    availability: 5,
+    category: "Mathematics",
+  },
+];
